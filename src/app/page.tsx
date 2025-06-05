7673a022ef73abe45f08e98765e02eaea12426dd
azhou2003 <91850567+azhou2003@users.noreply.github.com>
2025-06-05 02:18:01 -0500

    updated home page to handle metadata consistently

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import HomeClient from "./HomeClient";
+import { homeMetadata } from "../lib/metadata";
+
+export const metadata = homeMetadata;
 
 export default function Home() {
   return <HomeClient />;
